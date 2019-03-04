8a88babbe4bea74c8901cc167278e2abf9c0d61b
Viktor <vitek323pvi@rambler.ru>
2019-03-04 13:52:47 +0300

    Изменен gitignore

--- WorktimeProject/settings.py
+++ WorktimeProject/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'WorktimeProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'worktimeDb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
