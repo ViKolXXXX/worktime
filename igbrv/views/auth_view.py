1a87c5be3f9838cbc2932e1be7b55085b766effd
Viktor <vitek323pvi@rambler.ru>
2019-03-11 14:05:04 +0300

    Выполнена регистрация и вход для пользователей

--- igbrv/views/auth_view.py
+++ igbrv/views/auth_view.py
@@ -9,7 +9,7 @@ from django.views.generic.edit import FormView
 class RegisterFormView(FormView):
 
     form_class = UserCreationForm
-    success_url = "/"
+    success_url = "/login/"
     template_name = "register.html"
 
     def form_valid(self, form):
